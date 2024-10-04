43af6c2046a6d735c1712481766b012d495d1b4f
Sashiy1 <aleksander.paluch@wp.pl>
2024-10-04 20:59:16 +0200

    pages in work

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 
 import './App.css'
+import { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+// Використання lazy для динамічного імпорту компонентів
+const Home = lazy(() => import('./pages/Home'));
+const About = lazy(() => import('./pages/About'));
+const Contact = lazy(() => import('./pages/Contact'));
 
 function App() {
-
   return (
-    <>
-     
-    </>
-  )
+    <Router>
+      <Navbar />
+      {/* Suspense обгортає компоненти, які завантажуються динамічно */}
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
---
+++ src/pages/ReaserchPage/ReaserchPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ReaserchPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ReaserchPage
