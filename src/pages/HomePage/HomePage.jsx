ee7bb3c239c6f0c2f7d634e450ed4e52d3151d53
Sashiy1 <aleksander.paluch@wp.pl>
2024-10-07 19:13:28 +0200

    home page progress

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,16 +1,19 @@
 import Page from "../../components/Page/Page";
-
+import brain from "../../../assets/ai-artificial-intelligence-concept-face-outline-vector-24535527.png";
 import css from "./HomePage.module.css";
 
 const HomePage = () => {
   return (
-    <Page>
-      <h1>
-        Przewodnik po bezpiecznym i świadomym korzystaniu z AI na UW, obejmujący
-        zasady, etyczne wyzwania oraz praktyczne wskazówki dla studentów i
-        wykładowców.
-      </h1>
-    </Page>
+
+      <Page>
+        <h1 className={css.title}>
+          Przewodnik po bezpiecznym i świadomym korzystaniu z AI na UW,
+          obejmujący zasady, etyczne wyzwania oraz praktyczne wskazówki dla
+          studentów i wykładowców
+        </h1>
+        <img className={css.brain} src={brain} alt="" />
+      </Page>
+
   );
 };
 
