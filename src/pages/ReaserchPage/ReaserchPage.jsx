95f86e65fdbb92cc9a252f0257c97d6475924973
Sashiy1 <aleksander.paluch@wp.pl>
2024-10-08 00:55:24 +0200

    front fix

--- src/pages/ReaserchPage/ReaserchPage.jsx
+++ src/pages/ReaserchPage/ReaserchPage.jsx
@@ -5,14 +5,16 @@ import css from "./ReaserchPage.module.css";
 const ReaserchPage = () => {
   return (
     <>
-        <Page>
-      <h1>
-     Badanie
-      </h1>
-    </Page>
-    <Footer prevPage={"/rekomendacje"} prevPageName={"Rekomendacje"} nextPage={"/wytyczne"} nextPageName={"Wytyczne"} />
+      <Page>
+        <h1 className={css.title}>Badanie</h1>
+      </Page>
+      <Footer
+        prevPage={"/rekomendacje"}
+        prevPageName={"Rekomendacje"}
+        nextPage={"/wytyczne"}
+        nextPageName={"Wytyczne"}
+      />
     </>
-
   );
 };
 
