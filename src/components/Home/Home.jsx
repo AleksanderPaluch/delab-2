847a161326b7f5df64f43f04ddb62be2013a3393
Sashiy1 <aleksander.paluch@wp.pl>
2024-10-26 12:35:35 +0200

    reveal added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,23 @@
+import Reveal from "../Reveal/Reveal";
 import css from "./Home.module.css";
 
 const Home = () => {
   return (
     <section className={css.section}>
-      <h1 className={css.title}>
-        Przewodnik po bezpiecznym i świadomym korzystaniu z <span className={css.span1}>AI</span> na <span className={css.span2}>UW</span>, obejmujący
-        zasady, etyczne wyzwania oraz praktyczne wskazówki dla studentów i
-        wykładowców
-      </h1>
+      <Reveal>
+        <h1 className={css.title}>
+          Przewodnik po bezpiecznym i świadomym korzystaniu z{" "}
+          <span className={css.span1}>AI</span> na{" "}
+          <span className={css.span2}>UW</span>
+        </h1>
+      </Reveal>
+      <Reveal>
+        {" "}
+        <p>
+          Pbejmujący zasady, etyczne wyzwania oraz praktyczne wskazówki dla
+          studentów i wykładowców
+        </p>
+      </Reveal>
     </section>
   );
 };
