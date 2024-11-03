34920148484ed36d66a625c7f930731065cd6c6f
Sashiy1 <aleksander.paluch@wp.pl>
2024-11-03 17:07:18 +0100

    hero progress

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,27 +9,18 @@ const Hero = () => {
         <SideBarNav />
       </div>
 
-      <div className={css.content}>
-        <div>
-          <Reveal delay="0">
-            <h1 className={css.title}>Przewodnik po bezpiecznym i</h1>
-          </Reveal>
-          <Reveal>
-            <h1 className={css.title}>
-              świadomym korzystaniu z <span className={css.span}>AI</span> na{" "}
-              <span className={css.span}>UW</span>
-            </h1>
-          </Reveal>
-        </div>
+      <Reveal slide="false">
+        <h1 className={css.title}>Generatywna AI na UW - dobre praktyki</h1>
+      </Reveal>
 
-        <Reveal delay="0">
-          {" "}
-          <p className={css.text}>
-            Obejmujący zasady, etyczne wyzwania oraz praktyczne wskazówki dla
-            studentów i wykładowców
-          </p>
-        </Reveal>
-      </div>
+
+      <Reveal slide="false">
+        <p className={css.text}>
+        Przewodnik po bezpiecznym i
+        świadomym korzystaniu z AI, obejmujący zasady, etyczne wyzwania oraz praktyczne wskazówki dla
+          studentów i wykładowców
+        </p>
+      </Reveal>
     </section>
   );
 };
