0efa0512677b19d2f233b960ef5a829103824a21
Sashiy1 <aleksander.paluch@wp.pl>
2024-11-17 12:35:46 +0100

    no lazy test

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import { lazy } from "react";
 import "./App.css";
 import Layout from "./components/Layout/Layout";
-
 import Hero from "./components/Hero/Hero";
+import Raport from "./components/Raport/Raport";
+import Recomendations from "./components/Recomendations/Recomendations";
+import Research from "./components/Research/Research";
+import GenAI from "./components/GenAI/GenAI";
+import Tools from "./components/Tools/Tools";
+import Footer from "./components/Footer/Footer";
 
-const Raport = lazy(() => import("./components/Raport/Raport"));
-const Recomendations = lazy(() =>
-  import("./components/Recomendations/Recomendations")
-);
-const Research = lazy(() => import("./components/Research/Research"));
-const GenAI = lazy(() => import("./components/GenAI/GenAI"));
-const Tools = lazy(() => import("./components/Tools/Tools"));
-const Footer = lazy(() => import("./components/Footer/Footer"));
+// const Raport = lazy(() => import("./components/Raport/Raport"));
+// const Recomendations = lazy(() =>
+//   import("./components/Recomendations/Recomendations")
+// );
+// const Research = lazy(() => import("./components/Research/Research"));
+// const GenAI = lazy(() => import("./components/GenAI/GenAI"));
+// const Tools = lazy(() => import("./components/Tools/Tools"));
+// const Footer = lazy(() => import("./components/Footer/Footer"));
 
 function App() {
   return (
@@ -21,9 +26,9 @@ function App() {
         <Raport />
         <Recomendations />
         <Research />
-        {/* <GenAI /> */}
+        <GenAI />
         <Tools />
-        {/* <Footer /> */}
+        <Footer />
       </Layout>
     </>
   );
