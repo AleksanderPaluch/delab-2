3b2636ef3255a2500e7c8fb9380a64c9519fc6d5
Sashiy1 <aleksander.paluch@wp.pl>
2024-11-17 12:42:38 +0100

    no bar chart test

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
 import { lazy } from "react";
 import "./App.css";
 import Layout from "./components/Layout/Layout";
-import Hero from "./components/Hero/Hero";
-import Raport from "./components/Raport/Raport";
-import Recomendations from "./components/Recomendations/Recomendations";
-import Research from "./components/Research/Research";
-import GenAI from "./components/GenAI/GenAI";
-import Tools from "./components/Tools/Tools";
-import Footer from "./components/Footer/Footer";
 
-// const Raport = lazy(() => import("./components/Raport/Raport"));
-// const Recomendations = lazy(() =>
-//   import("./components/Recomendations/Recomendations")
-// );
-// const Research = lazy(() => import("./components/Research/Research"));
-// const GenAI = lazy(() => import("./components/GenAI/GenAI"));
-// const Tools = lazy(() => import("./components/Tools/Tools"));
-// const Footer = lazy(() => import("./components/Footer/Footer"));
+import Hero from "./components/Hero/Hero";
+
+const Raport = lazy(() => import("./components/Raport/Raport"));
+const Recomendations = lazy(() =>
+  import("./components/Recomendations/Recomendations")
+);
+const Research = lazy(() => import("./components/Research/Research"));
+const GenAI = lazy(() => import("./components/GenAI/GenAI"));
+const Tools = lazy(() => import("./components/Tools/Tools"));
+const Footer = lazy(() => import("./components/Footer/Footer"));
 
 function App() {
   return (
