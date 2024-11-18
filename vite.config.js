c1390e576cc989f6074a1b664e0fcd6d7251cb4b
Sashiy1 <aleksander.paluch@wp.pl>
2024-11-18 12:30:56 +0100

    vite initial

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Папка для збереження зібраного коду
-    assetsDir: 'assets', // Папка для статичних ресурсів (JS, CSS, зображення)
-    sourcemap: false, // Виключення sourcemaps у продакшн
-    cssCodeSplit: true, // Поділ CSS на окремі файли (оптимально для кешування)
-    minify: 'esbuild', // Швидка мініфікація через esbuild
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'], // Окремий файл для сторонніх бібліотек
-        },
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]',
-      },
-    },
-  },
-});
+})
