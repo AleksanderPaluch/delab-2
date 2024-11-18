4695c6ca0e22b55fad46c859ae4e5283b6c3811c
Sashiy1 <aleksander.paluch@wp.pl>
2024-11-18 11:34:50 +0100

    no props reveal

--- src/components/Reveal/Reveal.jsx
+++ src/components/Reveal/Reveal.jsx
@@ -1,15 +1,9 @@
 import { useEffect, useRef } from "react";
 import { motion, useAnimation, useInView } from "framer-motion";
 
-const Reveal = ({
-  children,
-  width = "fit-content",
-  slide = true,
-  amount = 0.25,
-  delay = 0.3
-}) => {
+const Reveal = ({ children, width = "fit-content" }) => {
   const ref = useRef(null);
-  const isInView = useInView(ref, { once: true, amount });
+  const isInView = useInView(ref, { once: true, amount: 0.25 });
   const mainControls = useAnimation();
   const slideControls = useAnimation();
 
@@ -29,32 +23,29 @@ const Reveal = ({
         }}
         initial="hidden"
         animate={mainControls}
-        transition={{ duration: 0.5, delay }}
+        transition={{ duration: 0.5, delay: 0.3 }}
       >
         {children}
       </motion.div>
-      {slide ? (
-        <motion.div
-          variants={{
-            hidden: { left: 0 },
-            visible: { left: "100%" },
-          }}
-          initial="hidden"
-          animate={slideControls}
-          transition={{ duration: 0.5, ease: "easeIn" }}
-          style={{
-            position: "absolute",
-            top: 4,
-            bottom: 4,
-            left: 0,
-            right: 0,
-            zIndex: 20,
-            background: "#00000011",
-          }}
-        ></motion.div>
-      ) : (
-        ""
-      )}
+
+      <motion.div
+        variants={{
+          hidden: { left: 0 },
+          visible: { left: "100%" },
+        }}
+        initial="hidden"
+        animate={slideControls}
+        transition={{ duration: 0.5, ease: "easeIn" }}
+        style={{
+          position: "absolute",
+          top: 4,
+          bottom: 4,
+          left: 0,
+          right: 0,
+          zIndex: 20,
+          background: "#00000011",
+        }}
+      ></motion.div>
     </div>
   );
 };
