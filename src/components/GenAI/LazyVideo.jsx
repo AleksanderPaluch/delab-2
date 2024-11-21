dc50a57f113c0cdd3046650d5848c6d6744798d2
Sashiy1 <aleksander.paluch@wp.pl>
2024-11-21 16:03:04 +0100

    video media added

--- src/components/GenAI/LazyVideo.jsx
+++ src/components/GenAI/LazyVideo.jsx
@@ -1,17 +1,33 @@
-
+import css from "./GenAI.module.css";
 
 const LazyVideo = () => {
   return (
-    <iframe
-      width={window.innerWidth < 768 ? "355" : "550"}
-      height={window.innerWidth < 768 ? "200" : "310"}
-      src="https://www.youtube.com/embed/i6ssUfaSbg4?si=QOsNeCMv918VAy9w"
-      title="YouTube video player"
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      referrerPolicy="strict-origin-when-cross-origin"
-      allowFullScreen
-    ></iframe>
+    <>
+      <div className={css.smallVideo}>
+        <iframe
+          width={355}
+          height={200}
+          src="https://www.youtube.com/embed/i6ssUfaSbg4?si=QOsNeCMv918VAy9w"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <div className={css.largeVideo}>
+        <iframe
+          width={550}
+          height={310}
+          src="https://www.youtube.com/embed/i6ssUfaSbg4?si=QOsNeCMv918VAy9w"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </>
   );
 };
 
